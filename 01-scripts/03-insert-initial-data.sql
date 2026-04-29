2e40de5236e314be1bef64c129f545db07e438e0
Juan M. Barrios <j.m.barrios@gmail.com>
2026-04-29 03:57:53 +0000

    Agrega carga de archivos csv

---
+++ 01-scripts/03-insert-initial-data.sql
@@ -0,0 +1,5 @@
+.import --csv --skip 1 00-data/raw/Portal_plots.csv Plots
+.import --csv --skip 1 00-data/raw/Portal_rodent_species.csv Species
+.import --csv --skip 1 00-data/raw/moon_dates.csv NewMoonDates
+.import --csv --skip 1 00-data/raw/Portal_rodent.csv Surveys
+.import --csv --skip 1 00-data/raw/Portal_rodent_trapping.csv Trapping
